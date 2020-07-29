5fd657f582c0621ea05207b0fae48167f8c65844
Marvel Dev <marvellousalika48@gmail.com>
2020-07-29 11:29:47 +0100

    -add countup for the clock. -add delete for entries.

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const inputTwo = document.querySelector('.input-two');
 const inputThree = document.querySelector('.input-three');
 const entriesContainer = document.querySelector('.entries-container');
 const button = document.querySelector('.button');
-const currentTime = document.querySelector('.time-content')
-currentTime.innerHTML = new Date().toLocaleTimeString();
+
+const time = setInterval(() => {
+  const currentTime = new Date().toLocaleTimeString();
+  document.querySelector('.time-content').innerHTML = currentTime; 
+})
 
 button.addEventListener('click',  () => {
   event.preventDefault();
@@ -21,12 +24,48 @@ button.addEventListener('click',  () => {
   } else {
     const output = `
       <section class="box2">
+        <button class="delete-button">X</button>
         <span class="span-output">WAKE UP TIME: ${inputOneValue} GMT</span>
         <span class="span-output">LUNCH TIME: ${inputTwoValue} GMT</span>
         <span class="span-output">NAP TIME: ${inputThreeValue} GMT</span>
       </section>
+      <section class="delete-container">
+        <div class="modal-container">
+          <p class="modal-content">ARE YOU SURE?</p>
+          <button type="button" class="confirm-button">YES</button>
+          <button type="button" class="cancel-button">NO</button>
+        </div>
+      </section>
     `
     entriesContainer.style.display = 'block';
     entriesContainer.innerHTML += output;
+
+    const deleteIcons = document.querySelectorAll('.delete-button');
+    for (let index = 0; index < deleteIcons.length; index++) {
+      const element = deleteIcons[index];
+      element.addEventListener('click', () => {
+        element.parentElement.nextElementSibling.style.display = 'block';
+      })
+    }
+
+    const confirmButtons = document.querySelectorAll('.confirm-button');
+    for (let index = 0; index < confirmButtons.length; index++) {
+      const confirmButton = confirmButtons[index];
+      confirmButton.addEventListener('click', () => {
+        const item = confirmButton.parentElement.parentElement.previousElementSibling;
+        const modal = confirmButton.parentElement.parentElement;
+        entriesContainer.removeChild(item);
+        modal.style.display = 'none';
+      })
+    }
+    
+    const cancelButtons = document.querySelectorAll('.cancel-button');
+    for (let index = 0; index < cancelButtons.length; index++) {
+      const cancelButton = cancelButtons[index];
+      cancelButton.addEventListener('click', () => {
+        const modal = cancelButton.parentElement.parentElement;
+        modal.style.display = 'none';
+      })
+    }
   };
 });
